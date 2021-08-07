c61d517db7f182ed96c0528c2d6bef6807f1d379
Parag <paragdurafe@outlook.com>
2021-08-07 21:36:56 +0530

    Property Area Extracted

--- Real_Estate.py
+++ Real_Estate.py
@@ -6,4 +6,5 @@ soup=BeautifulSoup(c,"html.parser")
 all=soup.find_all("div",{"class":"flex relative clearfix m-srp-card__container"})
 # print(all[0].find("div",{"class":"m-srp-card__price"}).text)
 for items in all:
-    print(items.find("div",{"class":"m-srp-card__price"}).text)
+    print(items.find("div",{"class","m-srp-card__price"}).text)
+    print(items.find("div",{"class","m-srp-card__summary__info"}).text)
