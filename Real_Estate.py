729844f54c19850b77ebf083bae99ddf30953ce6
Parag <paragdurafe@outlook.com>
2021-08-07 21:17:35 +0530

    Price Extracted

--- Real_Estate.py
+++ Real_Estate.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-req=requests.get("https://www.99acres.com/search/property/buy/residential-all/indore?search_type=QS&refSection=GNB&search_location=HP&lstAcn=HP_R&lstAcnId=0&src=CLUSTER&preference=S&selected_tab=1&city=142&res_com=R&property_type=R&isvoicesearch=N&keyword=indore&strEntityMap=IiI%3D&refine_results=Y&Refine_Localities=Refine%20Localities&action=%2Fdo%2Fquicksearch%2Fsearch&searchform=1&price_min=null&price_max=null")
+req=requests.get("https://www.magicbricks.com/property-for-sale/residential-real-estate?&proptype=Multistorey-Apartment,Builder-Floor-Apartment,Penthouse,Studio-Apartment,Residential-House,Villa&cityName=Indore")
 c=req.content
 soup=BeautifulSoup(c,"html.parser")
+all=soup.find_all("div",{"class":"flex relative clearfix m-srp-card__container"})
+# print(all[0].find("div",{"class":"m-srp-card__price"}).text)
+for items in all:
+    print(items.find("div",{"class":"m-srp-card__price"}).text)
